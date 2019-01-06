3f73f96318acd50010e1aa9a8fec37346d33c3a8
vodka <43000704+ripvodka@users.noreply.github.com>
2019-01-05 21:06:08 -0800

    Create FILE* object.c

---
+++ FILE* object.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+    char* fl_name;
+
+    printf("Enter a file name to read: ");
+
+   scanf("%s", fl_name);
+
+    if(strncmp(fl_name, "cat.txt", 7) == 0) {
+        FILE* ptr = fopen(fl_name, "r");
+
+        char c;
+
+        while((c = fgetc(ptr)) != EOF) {
+            printf("%c", c);
+        }
+
+    }
+
+    if(!strncmp(fl_name, "cat.txt", 7) == 0) {
+        printf("no input file found\n");
+    }
+
+    if(fl_name = NULL) {
+        printf("null\n");
+    }
+
+}
